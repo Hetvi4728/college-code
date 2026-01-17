febbe1491497da0492df0874315d075786eb2c04
patel vandan <vandan120ptl@gmail.com>
2026-01-17 14:55:18 +0530

    table

---
+++ table.cpp
@@ -0,0 +1,18 @@
+/*Prints the table*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number :";
+    cin>>n;
+
+    cout<<"Table of "<< n << ":~" <<endl;
+    for(int i=1; i<=10; i++)
+    {
+        cout<< n <<" x "<< i << " = " << n*i << endl;
+    }
+    return 0;
+}
